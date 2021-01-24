d3796a2d11f7dc649fadbce5bb641aab7616663c
GhaidEwawi <ghaid_99@hotmail.com>
2021-01-24 16:04:09 +0200

    Command line commands from script

---
+++ script.py
@@ -0,0 +1,6 @@
+# Automating terminal command
+# Excuting Shell command using code.
+import subprocess
+for i in range(5):
+    # The list contains the arguments of our comman line code.
+    subprocess.check_call(['python', 'example.py'])
